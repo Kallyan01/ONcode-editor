419b9f82fbc6e34d1c916f09a89a8956e723b2d8
Kallyan <singhakallyan@gmail.com>
2021-01-26 00:07:59 +0530

    comitted

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-function compile() {
+function compile() 
+ {
     var html = document.getElementById("html");
     var css = document.getElementById("css");
     var js = document.getElementById("js");
-    var code = document.getElementById("code").contentWindow.document;
-  
-    document.body.onkeyup = function() {
+    var code = document.getElementById("code").contentWindow.document;  
+    document.body.onkeyup = function() 
+    {
       code.open();
       code.writeln(
         html.value +
